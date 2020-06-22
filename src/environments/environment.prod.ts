8a485271f9ffb690c5e58376baeac3f01e75bef3
Rodrigo FERREIRA DE MORAIS <rferre01@Vision.local>
2020-06-22 20:16:51 -0300

    Config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
   urlback: 'http://localhost:3333',  ////'https://backendinfinity.herokuapp.com'
-  AuthKeyG: 'AIzaSyBVXVF1gzHjK0qaGjm2oURy4nPhgt8TbB8'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,4 @@
 export const environment = {
    production: false,
    urlback: "http://localhost:3333",
-   AuthKeyG: "AIzaSyBVXVF1gzHjK0qaGjm2oURy4nPhgt8TbB8"
 };
