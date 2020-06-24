5d1c5844649cfe4938c07f70e5bfeb602265bed5
Rodrigo FERREIRA DE MORAIS <rferre01@Vision.local>
2020-06-24 02:52:04 -0300

    Logo

--- src/app/cadastro-loja/cadastro-loja.component.ts
+++ src/app/cadastro-loja/cadastro-loja.component.ts
@@ -10,25 +10,39 @@ import { Router} from '@angular/router';
 export class CadastroLojaComponent implements OnInit {
 
   formData:  any ={} ;
-  errors: any= {}; 
+  errors: any= {};
+  file: File;
+  filename: any ="";
 
   constructor(private storeService: StoreService,
     private router: Router) { }
 
   ngOnInit(): void {
-
   }
  
+  onChange(event){
+     this.file = event.srcElement.files[0];
+     this.filename = event.srcElement.files[0].name;
+  }
+
   onLogoutClick(){
     this.storeService.logout();
   }
 
-  teste(){
-    console.log(this.formData);
-    this.storeService.cadastroloja(this.formData).subscribe(
+  onSubmit(){
+    const formDataM=new FormData();
+    formDataM.append('name', this.formData.name);
+    formDataM.append('description', this.formData.description);
+    formDataM.append('categoria', this.formData.categoria);
+    formDataM.append('lat', this.formData.lat);
+    formDataM.append('lon', this.formData.lon);
+    formDataM.append('file', this.file, this.filename);
+    console.log(formDataM);
+
+    this.storeService.cadastroloja(formDataM).subscribe(
       (resu)=> {
         alert("Cadastro atualizado com sucess!");
-        this.router.navigate(['/home', {registered: 'success'}])
+        this.router.navigate(['/home', {registered: 'sucesso'}])
       },
       (errorResponse)=> {
         this.errors = errorResponse.error;
